ba010f5a8594e6d2e9d1c158e64c41bc214a1eda
LockerUp <loockerup@gmail.com>
2024-02-01 16:29:43 +0700

    add comments

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from xgboost import XGBClassifier
 
+# загрузка датасета
 df = pd.read_csv('parkinsons.data')
-print(df.head())
 
+# извлечение объектов и меток
 features = df.loc[:, df.columns != 'status'].values[:, 1:]
 labels = df.loc[:, 'status'].values
 
+# нормализатор
 scaler = MinMaxScaler((-1, 1))
 X = scaler.fit_transform(features)
 
+# Разделите набор данных на обучающий(80%) и тестовый(20%) наборы.
 X_train, X_test, Y_train, Y_test = train_test_split(X, labels, test_size=0.2, train_size=0.8)
 
+# Инициализация классификатора XGBoost
 model = XGBClassifier()
-model.fit(X_test, Y_test)
+# Обучаем модель на обучающих данных
+model.fit(X_train, Y_train)
 
+# Делаем прогнозы и оцениваем точность
 Y_hat = [round(yhat) for yhat in model.predict(X_test)]
 print(accuracy_score(Y_test, Y_hat))
 
