89e6f62c47eb09e3bac7178f1a436c7b82e38ee7
LockerUp <loockerup@gmail.com>
2024-02-01 19:28:29 +0700

    finish

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import pandas as pd
-
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
 
-# загрузка датасета
+# Загрузка данных
 df = pd.read_csv('parkinsons.data')
+print(df.head())
 
-# извлечение объектов и меток
+# Выделение признаков и меток
 features = df.loc[:, df.columns != 'status'].values[:, 1:]
 labels = df.loc[:, 'status'].values
 
-# нормализатор
+# Нормализация признаков
 scaler = MinMaxScaler((-1, 1))
 X = scaler.fit_transform(features)
 
-# Разделите набор данных на обучающий(80%) и тестовый(20%) наборы.
+# Разделение данных на обучающий и тестовый наборы
 X_train, X_test, Y_train, Y_test = train_test_split(X, labels, test_size=0.2, train_size=0.8)
 
-# Инициализация классификатора XGBoost
+# Инициализация XGBoost классификатора
 model = XGBClassifier()
-# Обучаем модель на обучающих данных
-model.fit(X_train, Y_train)
 
-# Делаем прогнозы и оцениваем точность
-Y_hat = [round(yhat) for yhat in model.predict(X_test)]
+# Гиперпараметрическая настройка с помощью Grid Search
+param_grid = {
+    'learning_rate': [0.1, 0.01, 0.001],
+    'max_depth': [3, 5, 7],
+    'n_estimators': [100, 200, 300]
+}
+grid_search = GridSearchCV(model, param_grid, scoring='accuracy', cv=5)
+grid_search.fit(X_train, Y_train)
+best_model = grid_search.best_estimator_
+
+# Обучение модели на обучающих данных
+best_model.fit(X_train, Y_train)
+
+# Прогнозирование на тестовых данных и оценка точности
+Y_hat = best_model.predict(X_test)
 print(accuracy_score(Y_test, Y_hat))
-
